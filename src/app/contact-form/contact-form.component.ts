f2fc137ceae0bd5fd348ad09becc834e55861818
gmdmgithub <gmika@interia.pl>
2018-10-14 23:28:14 +0200

    first validation

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChange(ngModel){
+    console.log('ngModel',ngModel);
+    
+  }
+
+}
